807a57796b9e56c4f87bcdeb06fe2fc476c75fbf
Francisco Jr. B. Cenci <124214803+franciscocenci@users.noreply.github.com>
2024-08-21 23:20:29 +0000

    move `.env` file to `.env.development`

--- infra/database.js
+++ infra/database.js
@@ -2,7 +2,7 @@ import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
-    host: process.env.POSTGRESd_HOST,
+    host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
